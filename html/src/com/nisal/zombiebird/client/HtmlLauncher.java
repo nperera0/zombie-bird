72ef9fc036113ad2443c7f29d1f6654f4fcddafe
Nisal Perera <nisalperera0@gmail.com>
2015-08-27 12:02:12 -0400

    basic game framework

---
+++ html/src/com/nisal/zombiebird/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.nisal.zombiebird.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.nisal.zombiebird.ZBGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new ZBGame();
+        }
+}
